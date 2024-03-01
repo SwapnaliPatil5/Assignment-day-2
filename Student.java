47a5d6afcd982804b37916607ca7686f6c8b5f98
Swapnali Patil <152301536+SwapnaliPatil5@users.noreply.github.com>
2024-03-01 11:02:25 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -12,12 +12,12 @@ student.display();
 
 //constructor to assign initial values
 
-public Student(String name,int sub1,int sub2,int sub3)
+public Student(String n,int s1,int s2,int s3)
 {
-this.name= name;
-this.sub1 =sub1;
-this.sub2 = sub2;
-this.sub3 = sub3;
+this.name= n;
+this.sub1 =s1;
+this.sub2 = s2;
+this.sub3 = s3;
 }
 
 void computeTotalAverage()
